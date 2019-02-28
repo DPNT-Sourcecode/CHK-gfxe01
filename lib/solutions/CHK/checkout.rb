15e9912e5df776931ca2f099b639c3e0424b0314
sbx_user1059 <sbx_user1059@ip-10-128-59-61.eu-west-2.compute.internal>
2019-02-28 16:16:16 +0000

    Thu Feb 28 16:16:16 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,7 +2,22 @@
 class Checkout
 
   def checkout(skus)
-    raise 'Not implemented'
+    array_of_products = skus.sort.split('')
+    output = 0
+    array_of_products.map do |product|
+    	if product == 'A'
+    		output += 50
+    	elsif product == 'B'
+    		output += 30
+    	elsif product == 'C'
+    		output += 20
+    	elsif product == 'D'
+    		output += 15
+    	else return -1
+    	end
+    end
+    # How to implement discounts?
   end
 
 end
+
